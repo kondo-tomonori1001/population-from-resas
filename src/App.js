96e0c209159744c8fb203a892384a143c584d696
kondo tomonori <tomonorik1001@gmail.com>
2022-04-21 04:31:22 +0900

    feat: 自動デプロイをテスト

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!!!</p>
+        <p>自動デプロイをテスト！！</p>
         <a
           className="App-link"
           href="https://reactjs.org"
